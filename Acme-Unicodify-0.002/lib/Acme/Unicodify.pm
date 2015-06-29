c2a0a2effd726bf83f0ef2c233e7d464c997d03e
Joel Maslak <jmaslak@antelope.net>
2015-06-28 18:28:44 -0600

    Change version number required.

--- Acme-Unicodify-0.002/lib/Acme/Unicodify.pm
+++ Acme-Unicodify-0.002/lib/Acme/Unicodify.pm
@@ -12,26 +12,6 @@ use v5.22;
 use strict;
 use warnings;
 
-=head1 SYNOPSIS
-
-  my $translate = Acme::Unicodify->new();
-
-  $foo = $translate->to_unicode('Hello, World');
-  $bar = $translate->back_to_ascii($unified_string);
-
-  file_to_unicode('/tmp/infile', '/tmp/outfile');
-  file_back_to_ascii('/tmp/infile', '/tmp/outfile');
-
-=head1 DESCRIPTION
-
-This is intended to translate basic 7 bit ASCII into characters
-that use several Unicode features, such as accent marks and
-non-Latin characters.  While this can be used just for fun, a
-better use perhaps is to use it as part of a test suite, to
-allow you to easily pass in Unicode and determine if your system
-handles Unicode without corrupting the text.
-
-=cut
 
 use autodie;
 
@@ -93,11 +73,6 @@ my %_TRANSLATE = (
     Z => "\N{U+2621}"
 );
 
-=method new
-
-Create a new instance of the Unicodify object.
-
-=cut
 
 sub new {
     my $class = shift;
@@ -110,15 +85,6 @@ sub new {
     return $self;
 }
 
-=method to_unicode($str)
-
-Takes an input string and translates it into look-alike Unicode
-characters.  Input is any string.
-
-Basic ASCII leters are translated into Unicode "look alikes", while
-any character (Unicode or not) is passed through unchanged.
-
-=cut
 
 sub to_unicode {
     my $self = shift;
@@ -139,14 +105,6 @@ sub to_unicode {
     return NFD($out);
 }
 
-=method back_to_ascii($str)
-
-Takes an input string that has perhaps previously been produced
-by C<to_unicode> and translates the look-alike characters back
-into 7 bit ASCII.  Any other characters (Unicode or ASCII) are
-passed through unchanged.
-
-=cut
 
 sub back_to_ascii {
     my $self = shift;
@@ -167,20 +125,6 @@ sub back_to_ascii {
     return $out;
 }
 
-=method file_to_unicode($infile, $outfile)
-
-This method reads the file with the named passed as the first
-argument, and produces a new output file with the name passed
-as the second argument.
-
-The routine, as it reads the input file, will call C<to_unicode>
-on each line.
-
-Note this will overwrite existing files and it assumes the input
-and output files are in UTF-8 encoding (or plain ASCII in the
-case that no codepoints >127 are used).
-
-=cut
 
 sub file_to_unicode {
     if ($#_ != 2) { confess 'invalid call' }
@@ -201,20 +145,6 @@ sub file_to_unicode {
     return;
 }
 
-=method file_back_to_ascii($infile, $outfile)
-
-This method reads the file with the named passed as the first
-argument, and produces a new output file with the name passed
-as the second argument.
-
-The routine, as it reads the input file, will call C<back_to_ascii>
-on each line.
-
-Note this will overwrite existing files and it assumes the input
-and output files are in UTF-8 encoding (or plain ASCII in the
-case that no codepoints >127 are used).
-
-=cut
 
 sub file_back_to_ascii {
     if ($#_ != 2) { confess 'invalid call' }
@@ -250,3 +180,95 @@ sub _define_cache {
 
 1;
 
+__END__
+
+=pod
+
+=encoding UTF-8
+
+=head1 NAME
+
+Acme::Unicodify - Convert ASCII text into look-somewhat-alike unicode
+
+=head1 VERSION
+
+version 0.002
+
+=head1 SYNOPSIS
+
+  my $translate = Acme::Unicodify->new();
+
+  $foo = $translate->to_unicode('Hello, World');
+  $bar = $translate->back_to_ascii($unified_string);
+
+  file_to_unicode('/tmp/infile', '/tmp/outfile');
+  file_back_to_ascii('/tmp/infile', '/tmp/outfile');
+
+=head1 DESCRIPTION
+
+This is intended to translate basic 7 bit ASCII into characters
+that use several Unicode features, such as accent marks and
+non-Latin characters.  While this can be used just for fun, a
+better use perhaps is to use it as part of a test suite, to
+allow you to easily pass in Unicode and determine if your system
+handles Unicode without corrupting the text.
+
+=head1 METHODS
+
+=head2 new
+
+Create a new instance of the Unicodify object.
+
+=head2 to_unicode($str)
+
+Takes an input string and translates it into look-alike Unicode
+characters.  Input is any string.
+
+Basic ASCII leters are translated into Unicode "look alikes", while
+any character (Unicode or not) is passed through unchanged.
+
+=head2 back_to_ascii($str)
+
+Takes an input string that has perhaps previously been produced
+by C<to_unicode> and translates the look-alike characters back
+into 7 bit ASCII.  Any other characters (Unicode or ASCII) are
+passed through unchanged.
+
+=head2 file_to_unicode($infile, $outfile)
+
+This method reads the file with the named passed as the first
+argument, and produces a new output file with the name passed
+as the second argument.
+
+The routine, as it reads the input file, will call C<to_unicode>
+on each line.
+
+Note this will overwrite existing files and it assumes the input
+and output files are in UTF-8 encoding (or plain ASCII in the
+case that no codepoints >127 are used).
+
+=head2 file_back_to_ascii($infile, $outfile)
+
+This method reads the file with the named passed as the first
+argument, and produces a new output file with the name passed
+as the second argument.
+
+The routine, as it reads the input file, will call C<back_to_ascii>
+on each line.
+
+Note this will overwrite existing files and it assumes the input
+and output files are in UTF-8 encoding (or plain ASCII in the
+case that no codepoints >127 are used).
+
+=head1 AUTHOR
+
+Joel Maslak <jmaslak@antelope.net>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2015 by Joel Maslak.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
